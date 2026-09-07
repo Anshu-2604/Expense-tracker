facea9586dadec42a9887e93380060c26c7bbbd6
Anshu-2604 <anshukumar41419@gmail.com>
2026-09-08 02:08:22 +0530

    Cors fix

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const cookieParser=require("cookie-parser")
 const app = express();
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://expense-tracker-eta-lyart-50.vercel.app",
     credentials: true
 }))
 app.use(express.json());
