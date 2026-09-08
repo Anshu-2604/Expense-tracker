ec44f7538c48a1d138192ef57df170d726f308a6
Anshu-2604 <anshukumar41419@gmail.com>
2026-09-08 14:57:50 +0530

    problem fixes

--- frontend/my-app/src/assets/Dashboard.jsx
+++ frontend/my-app/src/assets/Dashboard.jsx
@@ -14,7 +14,7 @@ function Dashboard() {
     useEffect(() => {
         async function dashboard() {
             try {
-                const response = await fetch("https://expense-tracker-c2f4.onrender.com//dashboard",{
+                const response = await fetch("https://expense-tracker-c2f4.onrender.com/dashboard",{
                     credentials:'include'
                 });
 
