59fbee713d12025896287cceb60851e56a4c3558
Anshu-2604 <anshukumar41419@gmail.com>
2026-09-08 14:13:59 +0530

    Token fix

--- backend/server.js
+++ backend/server.js
@@ -90,7 +90,11 @@ app.post("/Login" ,async function(req,res){
                     }
                     
                 )
-                    res.cookie("token",token)
+                   res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+});
                     return res.status(200).json({message:"login successfully"})
                 }
             })
